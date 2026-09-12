9024acc8290eac237b9ab1f81ed8e1dc79d465e1
kirillmehopov-design <kirillmehopov@gmail.com>
2026-09-12 16:18:24 +0500

    Add Task 2 next day checker

--- main.dart
+++ main.dart
@@ -7,4 +7,48 @@ void main() {
     }
     print('');
   }
+
+  print('TASK 2');
+
+  int day = 28;
+  int month = 2;
+  int year = 2026;
+
+  bool leapYear =
+      year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+
+  List<int> daysInMonth = [
+    31,
+    leapYear ? 29 : 28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31
+  ];
+
+  if (month < 1 || month > 12 || day < 1 || day > daysInMonth[month - 1]) {
+    print('invalid date');
+  } else {
+    day++;
+
+    if (day > daysInMonth[month - 1]) {
+      day = 1;
+      month++;
+    }
+
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+
+    String nextDay = day.toString().padLeft(2, '0');
+    String nextMonth = month.toString().padLeft(2, '0');
+    print('$nextDay.$nextMonth.$year');
+  }
 }
